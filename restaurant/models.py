fc8c00fe75a2d2c13be9d12d86d774242aefb0d2
Jihad Hamdan <170100844+JihadHamdan-Tech@users.noreply.github.com>
2025-09-17 14:23:12 +0300

    Add menu and Booking APIs with routers and Viewsets

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length =255)
+    no_of_guests =models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+    def __str__(self):
+     return f"{self.name} - {self.BookingDate}"
+ 
+ 
+ 
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price=models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField()
+    
+    
+    
+    
+    def __str__(self):
+        return f"{self.Title} - {self.Price}"
