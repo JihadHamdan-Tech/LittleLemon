e4faed8ccb2015489c90996f55ae926540af8e2f
Jihad Hamdan <170100844+JihadHamdan-Tech@users.noreply.github.com>
2025-09-21 16:33:09 +0300

    Add auth endpoint and update permissions

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
-from rest_framework import viewsets, permissions
+from rest_framework import generics, viewsets, permissions
 from .models import Menu, Booking
 from .serializers import MenuSerializer,BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 
 # Create your views here.
 def index(request):
     return render(request,'restaurant/index.html',{} )
 
 class MenuItemsView(generics.ListCreateAPIView):
-    
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     
     
 class BookingViewSet(viewsets.ModelViewSet):
-    
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
